d3f9597e63655d67ea19b30a2d49d2eaf0904091
Lucas Smallcomb <47441377+Lucas-Smallcomb@users.noreply.github.com>
2019-02-18 13:46:55 -0500

    Add files via upload
    Dataframe descriptions added

--- Filter.py
+++ Filter.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[3]:
 
 
 import pandas as pd
 import os
 
+# Creates a dataframe from a .csv file
 file_path = "C:/Users/nobody/Documents/Microsoft Malware Prediction Datasets/train.csv"
 data = pd.read_csv(file_path)
 
-print(type(data))
+
+# In[4]:
+
+
+# Removes a column "Census_IsPenCapable" if run
+data.drop("Census_IsPenCapable", axis = 1, inplace = True)
+
+
+# In[5]:
+
+
+# Shows full dataframe (truncated)
+data
 
 
 # In[7]:
 
 
-data.drop("Census_IsPenCapable", axis = 1, inplace = True)
+import numpy
+
+# Describes the dataframe filtering for numeric types
+data.describe(include = numpy.number)
 
 
 # In[8]:
 
 
-data
+# Describes the dataframe filtering for objects
+data.describe(include = numpy.object)
 
